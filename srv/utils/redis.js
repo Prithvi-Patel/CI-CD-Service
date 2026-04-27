0e5f7497fb24f9b45e4fa8dd288f0319f0dd4daf
Prithvi Patel C S <prithvipatel.c@laderatechnology.com>
2026-04-27 04:29:47 +0000

    done

--- srv/utils/redis.js
+++ srv/utils/redis.js
@@ -1,12 +1,9 @@
 import { createClient } from 'redis';
-
 let client;
-
 export async function getRedis() {
     if (client) return client;
 
     let redisUrl;
-
     // Get from BTP (VCAP_SERVICES)
     if (process.env.VCAP_SERVICES) {
         const vcap = JSON.parse(process.env.VCAP_SERVICES);
@@ -15,12 +12,10 @@ export async function getRedis() {
             redisUrl = vcap['redis-cache'][0].credentials.uri;
         }
     }
-
     // Local fallback
     if (!redisUrl) {
         redisUrl = process.env.REDIS_URL;
     }
-
     client = createClient({
         url: redisUrl,
         socket: {
@@ -32,7 +27,6 @@ export async function getRedis() {
     client.on('error', (err) => {
         console.error('Redis Error:', err);
     });
-
     await client.connect();
     console.log('--------Redis Connected---------------');
 
